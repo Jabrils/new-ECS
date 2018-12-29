5bd1d23a39d37edd6ec7fa3dfa7ac652b3e555f7
Jabrils <ankelzspam@gmail.com>
2018-12-28 16:44:56 -0800

    init_commit

---
+++ Assets/MoveSpeedComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace HaxwareRTS
+{
+    [Serializable]
+    public struct MoveSpeed : IComponentData
+    {
+        public float Value;
+    }
+
+    [UnityEngine.DisallowMultipleComponent]
+    public class MoveSpeedComponent : ComponentDataWrapper<MoveSpeed>
+    {
+    }
+}
---
+++ Assets/MovementSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using HaxwareRTS;
+
+public class MovementSystem : JobComponentSystem
+{
+    [ComputeJobOptimization]
+    struct MovementJob : IJobProcessComponentData <Position, MoveSpeed>
+    {
+        public float deltaTime;
+
+        public void Execute(ref Position pos, [ReadOnly] ref MoveSpeed spd)
+        {
+            float3 val = pos.Value;
+
+            val += deltaTime * spd.Value;
+
+            pos.Value = val;
+        }
+    }
+
+    // 
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        MovementJob mJob = new MovementJob
+        {
+            deltaTime = Time.deltaTime
+        };
+
+        return mJob.Schedule(this, 64, inputDeps);
+    }
+}
